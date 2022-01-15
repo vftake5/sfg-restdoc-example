a0e46acaad77eea30fca7bdbf4b50fe332c0b1d6
ViFTake5 <viftake5@gmail.com>
2022-01-15 17:02:51 +0100

    Initial commit

---
+++ src/main/java/guru/springframework/sfgrestdocexample/SfgRestdocExampleApplication.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgrestdocexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgRestdocExampleApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SfgRestdocExampleApplication.class, args);
+	}
+
+}
